bfe3505886f9de1a02369456d422e086973c6f87
Omar Elmenawi <omar.menawi@gmail.com>
2023-09-08 18:35:29 -0400

    s

---
+++ Events/Add/addEvents.js
@@ -0,0 +1,9 @@
+const gameDescription = `
+- Tile Change Color;
+- Player Click correct tile , player score +1;
+- Player wrong tile, player health -1;
+- Tiles become inactive for a period after incorrect option;
+`;
+console.log(gameDescription);
+
+//
